08c3ea306678f7f6bc8a1fc615317b7973088dc4
rathnakumar05 <rathnak010@gmail.com>
2024-01-19 16:39:25 +0530

    db optimization

--- utils/db.py
+++ utils/db.py
@@ -1,8 +1,19 @@
 import os
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 def get_sessionmaker():
-    engine = create_async_engine(url=os.getenv('MYSQL_URL'), echo=False)
-    sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
+    engine = create_async_engine(
+        url=os.getenv('MYSQL_URL'), 
+        echo=False,
+        pool_pre_ping=True,  
+        pool_recycle=1800,  
+        pool_size=10,
+        max_overflow=20,
+    )
+    sessionmaker = async_sessionmaker(
+        engine, 
+        expire_on_commit=False,
+        class_=AsyncSession,
+    )
 
     return sessionmaker
